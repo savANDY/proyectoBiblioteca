ba1dd70caaafd8a5d897bc23eb8fd120eb69698e
savandy <andicuu@gmail.com>
2017-03-14 13:38:51 +0100

    Creada ventana principal

--- ProyectoBiblioteca/src/vista/Principal.java
+++ ProyectoBiblioteca/src/vista/Principal.java
@@ -6,6 +6,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Color;
 
 public class Principal extends JFrame {
 
@@ -32,11 +41,45 @@ public class Principal extends JFrame {
 	 */
 	public Principal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(600, 300, 390, 325);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JButton button = new JButton("Gestion Socios");
+		button.setBounds(29, 99, 157, 77);
+		
+		JButton button_1 = new JButton("Gestion Libros");
+		button_1.setBackground(Color.DARK_GRAY);
+		button_1.setBounds(196, 11, 157, 77);
+		
+		
+		
+		JButton btnPrestamos = new JButton("Prestamos");
+		btnPrestamos.setBounds(196, 187, 157, 77);
+		btnPrestamos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		contentPane.setLayout(null);
+		contentPane.add(btnPrestamos);
+		contentPane.add(button_1);
+		contentPane.add(button);
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(Principal.class.getResource("/imagenes/prestamos.jpg")));
+		label.setBounds(29, 187, 157, 77);
+		contentPane.add(label);
+		
+		JLabel label_1 = new JLabel("");
+		label_1.setIcon(new ImageIcon(Principal.class.getResource("/imagenes/libros.jpg")));
+		label_1.setBounds(29, 11, 157, 77);
+		contentPane.add(label_1);
+		
+		JLabel label_2 = new JLabel("");
+		label_2.setIcon(new ImageIcon(Principal.class.getResource("/imagenes/socios.jpg")));
+		label_2.setBounds(196, 99, 157, 77);
+		contentPane.add(label_2);
 	}
-
 }
